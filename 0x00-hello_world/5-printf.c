0fb12c5604eddc0fe6561fbe904588966d4c7696
mysubbacc <mysubbacc@gmail.com>
2024-02-15 07:25:00 -0800

    done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
-  * main - prints out propermgrammer
-  * Return: return 0;
-  */
-
+* * main - Entry point
+* * Return: Always 0 (Success)
+*/
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art \n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
