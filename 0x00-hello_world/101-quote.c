c30e656f80d913a912a154779ce54f513f9e545c
mysubbacc <mysubbacc@gmail.com>
2024-02-16 01:15:18 -0800

    push

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,12 @@
 #include <unistd.h>
 
 /**
-  * main-print to streer
-  * Return: returns 1
-  */
+ * * main-Entry point
+ * * Return: 0 if successful
+ */
 
 int main(void)
 {
-	write(1, "and that piece of art is useful\n" - Dora Korpar, 2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
