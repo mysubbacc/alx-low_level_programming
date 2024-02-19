7eadeeac29f6bd8afdb08a2f5398d47bdb4040db
mysubbacc <mysubbacc@gmail.com>
2024-02-19 03:53:41 -0800

    again

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,21 +6,25 @@
  */
 int main(void)
 {
-    int nom1 = 0;
-    while (nom1 < 9) {
-        int nom2 = nom1 + 1;
-        while (nom2 < 10) {
-            putchar(nom1 + '0');
-            putchar(nom2 + '0');
-            if (nom1 != 8 || nom2 != 9) {
-                putchar(',');
-                putchar(' ');
-            }
-            nom2++;
-        }
-        nom1++;
-    }
-    putchar('\n');
-    return (0);
+	int nom1 = 0;
+
+	while (nom1 < 9)
+	{
+		int nom2 = nom1 + 1;
+
+		while (nom2 < 10)
+		{
+			putchar(nom1 + '0');
+			putchar(nom2 + '0');
+			if (nom1 != 8 || nom2 != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			nom2++;
+		}
+		nom1++;
+	}
+	putchar('\n');
+	return (0);
 }
-1
