e0c978c510539e519c6ef90b18863feaf7f449a8
mysubbacc <em.ogbonaya@stu.unizik.edu.ng>
2024-02-21 14:15:49 +0100

    trial

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * printputchar- prints _putchar
+ * Return: 0
+ */
+
+int printputchar(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}
+
+
