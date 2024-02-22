4d3e647eee66d41639c9ccefc1dbee781ba3e131
mysubbacc <em.ogbonaya@stu.unizik.edu.ng>
2024-02-22 21:37:08 +0100

    push

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit- print number last digit
+ * @n is the number
+ * Return: 0
+ */
+
+int print_last_digit(int n)
+{
+	int r;
+	r = n % 10;
+	return (r);
+}
