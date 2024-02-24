e9745cc6fcd612db2a7848c4670364e28647ca23
mysubbacc <em.ogbonaya@stu.unizik.edu.ng>
2024-02-24 20:40:40 +0100

    done

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main print out first 50 fibonacci series
+ * @i: iterate through the loop
+ * @a: hold first value
+ * @b:second value
+ * Return: 0
+ */
+
+int main(void)
+{
+        int i;
+        unsigned long int a = 0;
+        unsigned long int b = 1;
+
+        for (i = 1; i <= 98; i++)
+        {
+                unsigned long int j, k, l;
+                j = a;
+                k = b;
+                l = j + k;
+                if (i < 98)
+                        printf("%lu, ", l);
+                else
+                        printf("%lu", l);
+                a = k;
+                b = l;
+        }
+        printf("\n");
+        return (0);
+}
+				
