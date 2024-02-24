1d91704c27d13e6f7fe38dc9421a4c5e4cc5e629
mysubbacc <em.ogbonaya@stu.unizik.edu.ng>
2024-02-24 09:28:21 +0100

    nih

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,16 +8,18 @@
 
 int main(void)
 {
-	int i;
+	unsigned long int i;
+	unsigned long int sum = 0;
 
 	for (i = 0; i < 1024; i++)
 	{
 		if ((i % 3 == 0) || (i % 5 == 0))
 		{
-			i += i;
-			printf("%d", i);
+			sum += i;
 		}
+		
 	}
+	printf("%lu\n", sum);
 	return (0);
 }
 
