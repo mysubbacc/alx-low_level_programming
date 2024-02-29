266bdb402253b9ee6dfee821319d4fca2f8879fa
mysubbacc <em.ogbonaya@stu.unizik.edu.ng>
2024-02-29 11:00:39 +0100

    gewaork

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul-multiply two integers
+ * @a: is first intger
+ * @b: is secod integer
+ * Return: 0.
+ */
+
+int mul(int a, int b)
+{
+	int pro;
+
+	pro = a * b;
+
+	return (pro);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,5 +2,10 @@
 #define HEADER_FILE
 
 int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+int _putchar(char c);
+void print_most_numbers(void);
 
 #endif
