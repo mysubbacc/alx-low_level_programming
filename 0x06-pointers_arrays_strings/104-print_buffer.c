f021499b2b74d905b8c4097b97b62a6709d6bbf7
mysubbacc <mysubbacc@gmail.com>
2024-04-09 04:55:08 -0700

    git

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -17,50 +17,51 @@
 void print_buffer(char *b, int size)
 {
 	int i, j;
-    if (size <= 0)
-    {
-        printf("\n");
-        return;
-    }
-   
 
-    for (i = 0; i < size; i += 10)
-    {
-        printf("%08x: ", i);
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
 
-        for (j = i; j < i + 10; j += 2)
-        {
-            if (j < size)
-            {
-                printf("%02x", (unsigned char)b[j]);
-                if (j + 1 < size)
-                {
-                    printf("%02x ", (unsigned char)b[j + 1]);
-                }
-                else
-                {
-                    printf("   ");
-                }
-            }
-            else
-            {
-                printf("     ");
-            }
-        }
 
-        printf(" ");
-        for (j = i; j < i + 10 && j < size; j++)
-        {
-            if (b[j] >= 32 && b[j] <= 126)
-            {
-                printf("%c", b[j]);
-            }
-            else
-            {
-                printf(".");
-            }
-        }
+	for (i = 0; i < size; i += 10)
+	{
+		printf("%08x: ", i);
 
-        printf("\n");
-    }
+		for (j = i; j < i + 10; j += 2)
+		{
+			if (j < size)
+			{
+				printf("%02x", (unsigned char)b[j]);
+				if (j + 1 < size)
+				{
+					printf("%02x ", (unsigned char)b[j + 1]);
+				}
+				else
+				{
+					printf("   ");
+				}
+			}
+			else
+			{
+				printf("     ");
+			}
+		}
+
+		printf(" ");
+		for (j = i; j < i + 10 && j < size; j++)
+		{
+			if (b[j] >= 32 && b[j] <= 126)
+			{
+				printf("%c", b[j]);
+			}
+			else
+			{
+				printf(".");
+			}
+		}
+
+		printf("\n");
+	}
 }
