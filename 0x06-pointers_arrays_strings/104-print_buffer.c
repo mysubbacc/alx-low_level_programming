c5a2fc86202018182b654a07183df54966d7806e
mysubbacc <mysubbacc@gmail.com>
2024-04-09 08:38:10 -0700

    fr

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,81 +2,59 @@
 #include <stdio.h>
 
 /**
- * print_buffer - Prints the content of a buffer
- * @b: Pointer to the buffer
- * @size: Size of the buffer
+ * print_line - prints a s bytes of a buffer
+ * @c: buffer to print
+ * @s: bytes of buffer to print
+ * @l: line of buffer to print
  *
- * The function prints the content of size bytes of the buffer pointed by b.
- * The output should print 10 bytes per line. Each line starts with the
- * position of the first byte of the line in hexadecimal (8 chars), starting
- * with 0. Each line shows the hexadecimal content (2 chars) of the buffer, 2
- * bytes at a time, separated by a space. Each line shows the content of the
- * buffer. If the byte is a printable character, print the letter, if not,
- * print '.'. Each line ends with a new line \n. If size is 0 or less, the
- * output should be a new line only \n.
+ * Return: void
+ */
+void print_line(char *c, int s, int l)
+{
+	int j, k;
+
+	for (j = 0; j <= 9; j++)
+	{
+		if (j <= s)
+			printf("%02x", c[l * 10 + j]);
+		else
+			printf("  ");
+		if (j % 2)
+			putchar(' ');
+	}
+	for (k = 0; k <= s; k++)
+	{
+		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
+			putchar(c[l * 10 + k]);
+		else
+			putchar('.');
+	}
+}
+
+/**
+ * print_buffer - prints a buffer
+ * @b: buffer to print
+ * @size: size of buffer
+ *
+ * Return: void
  */
 void print_buffer(char *b, int size)
 {
 	int i;
 
-    if (size <= 0)
-    {
-        printf("\n");
-        return;
-    }
-
-    for (i = 0; i < size; i += 10)
-    {
-        print_hex_line(b, i, size);
-        print_ascii_line(b, i, size);
-        printf("\n");
-    }
-}
-
-void print_hex_line(char *b, int start, int size)
-{
-	int j;
-
-    printf("%08x: ", start);
-
-    for (j = start; j < start + 10; j += 2)
-    {
-        if (j < size)
-        {
-            printf("%02x", (unsigned char)b[j]);
-            if (j + 1 < size)
-            {
-                printf("%02x ", (unsigned char)b[j + 1]);
-            }
-            else
-            {
-                printf("   ");
-            }
-        }
-        else
-        {
-            printf("     ");
-        }
-    }
-}
-
-void print_ascii_line(char *b, int start, int size)
-{
-	int j;
-    printf(" ");
-    for (j = start; j < start + 10 && j < size; j++)
-    {
-        if (b[j] >= 32 && b[j] <= 126)
-        {
-            printf("%c", b[j]);
-        }
-        else
-        {
-            printf(".");
-        }
-    }
-    for (j = size - start; j < 10; j++)
-    {
-	    printf(" ");
-    }
+	for (i = 0; i <= (size - 1) / 10 && size; i++)
+	{
+		printf("%08x: ", i * 10);
+		if (i < size / 10)
+		{
+			print_line(b, 9, i);
+		}
+		else
+		{
+			print_line(b, size % 10 - 1, i);
+		}
+		putchar('\n');
+	}
+	if (size == 0)
+		putchar('\n');
 }
