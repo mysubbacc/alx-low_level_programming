420c865a122541818d29aedff311e693cc6f184c
mysubbacc <mysubbacc@gmail.com>
2024-05-18 07:06:04 -0700

    try again

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,43 +1,54 @@
 #include "main.h"
 
 /**
-  * is_plindrome - is an helper function
-  * @s: hold the string
-  * @start: start of a string
-  * @end: end of string length
-  * Return: 0 or 1
-  */
-
+ * is_plindrome - helper function to check for palindrome recursively
+ * @s: the string
+ * @start: starting index
+ * @end: ending index
+ * 
+ * Return: 1 if palindrome, 0 otherwise
+ */
 int is_plindrome(char *s, int start, int end)
 {
-	if (start >= end)
-	{
-		return (1);
-	}
-	if (s[start] != s[end])
-	{
-		return (0);
-	}
-	return (is_plindrome(s, start + 1, end - 1));
+    if (start >= end)
+    {
+        return (1);
+    }
+    if (s[start] != s[end])
+    {
+        return (0);
+    }
+    return (is_plindrome(s, start + 1, end - 1));
 }
-/**
-  * is_palindrome - check if a charter string is palindrone
-  * @s: holds the string
-  * Return: 0 or 1
-  */
 
+/**
+ * string_length - helper function to calculate the length of a string recursively
+ * @s: the string
+ * 
+ * Return: the length of the string
+ */
+int string_length(char *s)
+{
+    if (*s == '\0')
+    {
+        return (0);
+    }
+    return (1 + string_length(s + 1));
+}
+
+/**
+ * is_palindrome - check if a string is a palindrome
+ * @s: the string to check
+ * 
+ * Return: 1 if palindrome, 0 otherwise
+ */
 int is_palindrome(char *s)
 {
-	int i = 0;
+    int length = string_length(s);
 
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	if (i == 0)
-	{
-		return (1);
-	}
-
-	return (is_plindrome(s, 0, i - 1));
+    if (length == 0)
+    {
+        return (1); /* An empty string is considered a palindrome */
+    }
+    return (is_plindrome(s, 0, length - 1));
 }
