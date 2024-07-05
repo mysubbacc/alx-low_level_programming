2c8b62dc8871cb8f416b83c72cdf916ab91eb3f4
mysubbacc <em.ogbonnaya@stu.unizik.edu.ng>
2024-07-05 13:41:21 +0100

    dine

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,20 +1,16 @@
 #include <stdio.h>
+#define UNUSED(x) (void)(x)
 
 /**
- * main - check the main pro
- * @argc: number
- * @argv: vector
- * Return: o
+ * main - prints it name
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 if success, or any other number on fail
  */
-
 int main(int argc, char *argv[])
 {
-	if (argc < 2)
-	{
-		printf("%s\n", argv[0]);
-		return 1;
-	}
-
-	printf("%s\n", argv[1]);
-	return 0;
+	UNUSED(argc);
+	printf("%s\n", argv[0]);
+	return (0);
 }
