1ec2ae7cc27e170d17b0989f6787b27b445c0125
mysubbacc <em.ogbonnaya@stu.unizik.edu.ng>
2024-08-07 10:53:52 +0100

    git eat

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - multiply two numbers together
+ * @argc: arguement count
+ * @argv: arguement vector
+ * Return: 0 or 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int mul;
+
+	if (argc > 2)
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+
+		printf("%d\n", mul);
+	}
+
+	else
+	{
+		printf("Error\n");
+
+		return (1);
+	}
+
+	return (0);
+
+}
