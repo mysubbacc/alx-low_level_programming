4417c6e92bee5b758d882871641cd69ac6f79f6c
mysubbacc <em.ogbonnaya@stu.unizik.edu.ng>
2024-08-18 08:40:56 -0700

    gfua

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -15,70 +15,39 @@ char **strtow(char *str)
 	char **words;
 
 	if (str == NULL || *str == '\0')
-	{
 		return (NULL);
-	}
-
 	for (i = 0; str[i]; i++)
 	{
 		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
-		{
 			word_count++;
-		}
-
 	}
 	if (word_count == 0)
-	{
 		return (NULL);
-	}
-
 	words = (char **)malloc((word_count + 1) * sizeof(char *));
-
 	if (words == NULL)
-	{
 		return (NULL);
-	}
-
 	for (i = 0, k = 0; k < word_count; k++)
 	{
 		while (str[i] == ' ')
-		{
 			i++;
-		}
-
 		j = i;
-
 		while (str[j] != ' ' && str[j] != '\0')
-		{
 			j++;
-		}
-
 		word_len = j - i;
-
 		words[k] = (char *)malloc((word_len + 1) * sizeof(char));
-
 		if (words[k] == NULL)
 		{
 			for (l = 0; l < k; l++)
-			{
 				free(words[l]);
-			}
-
 			free(words);
 			return (NULL);
 		}
-
 		for (m = 0; m < word_len; m++)
-		{
 			words[k][m] = str[i + m];
-		}
 
 		words[k][word_len] = '\0';
-
 		i = j;
 	}
-
 	words[k] = NULL;
-
 	return (words);
 }
