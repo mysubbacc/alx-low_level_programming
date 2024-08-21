1204a4974a9c4000f363729526e5043e8631a4c0
mysubbacc <em.ogbonnaya@stu.unizik.edu.ng>
2024-08-21 05:48:51 -0700

    next

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,7 +15,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *str;
 	unsigned int i, j;
-	unsigned len1 = 0, len2 = 0;
+	unsigned int len1 = 0, len2 = 0;
 
 	if (s1 == NULL)
 		s1 = "";
